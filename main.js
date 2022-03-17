4d6af17855c7d8e0283b0e7f3dfd0677246cd7e8
al-duncan <dev@theduncans.uk>
2022-03-17 12:35:09 +0000

    javascript test

---
+++ main.js
@@ -0,0 +1,23 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+    getVisitCount();
+});
+
+const functionApi = 'https://crcgetcounter.azurewebsites.net/api/crcgetcounter';
+
+const getVisitCount = () => {
+    let count = 30;
+    fetch(functionApi)
+    .then(response => {
+        return response.text()
+    })
+    .then(response => {
+        console.log("Website called function API.",response);
+        count = response;
+        document.getElementById('counter').innerText = count;
+    }).catch(function(error) {
+        console.log(error);
+      });
+    return count;
+}
+
+getVisitCount();
